ce85fa7b651184e06d3846e3ece377c1cfa532f1
uplukaszp <uplukaszp1@gmail.com>
2017-01-22 00:31:16 +0100

    fixed layoutManager & added color previev

--- src/CUSTOMCOLORS/Color_Choosing_Panel.java
+++ src/CUSTOMCOLORS/Color_Choosing_Panel.java
@@ -4,12 +4,14 @@ import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Color_Choosing_Panel extends JPanel{
 
 	private JLabel htext,stext,btext;
 	private JSlider hslider,sslider,bslider;
-	private Color c;
+	private Color color;
 	public Color_Choosing_Panel() {
 		setBorder(new TitledBorder("Choose the color"));
 		
@@ -20,49 +22,67 @@ public class Color_Choosing_Panel extends JPanel{
 		hslider=new JSlider(0,360,180);
 		sslider=new JSlider(0,100,50);
 		bslider=new JSlider(0,100,50);
-		
+		hslider.addChangeListener(new Listener());
+		sslider.addChangeListener(new Listener());
+		bslider.addChangeListener(new Listener());
 		setLayout(new GridBagLayout());
 		GridBagConstraints c=new GridBagConstraints();
 		c.weightx=1.0;
-		c.weighty=1.0;
-		c.fill=GridBagConstraints.HORIZONTAL;
-		c.anchor=GridBagConstraints.NORTHWEST;
+		c.weighty=0;
+		//c.fill=GridBagConstraints.HORIZONTAL;
+		c.anchor=GridBagConstraints.EAST;
+		
 		c.gridx=0;
 		c.gridy=0;
 		add(htext,c);
-		c.gridx++;
-		add(hslider,c);
-		
-		c=new GridBagConstraints();
-		c.weightx=1.0;
-		c.weighty=1.0;
-		c.fill=GridBagConstraints.HORIZONTAL;
-		c.anchor=GridBagConstraints.NORTHWEST;
-		c.gridx=0;
-		c.gridy=1;
+		c.gridy++;
 		add(stext,c);
-		c.gridx++;
-		add(sslider,c);
-		
-		c=new GridBagConstraints();
-		c.weightx=1.0;
-		c.weighty=1.0;
-		c.fill=GridBagConstraints.HORIZONTAL;
-		c.anchor=GridBagConstraints.NORTHWEST;
-		c.gridx=0;
-		c.gridy=2;
+		c.gridy++;
 		add(btext,c);
-		c.gridx++;
+		c.gridx=1;
+		c.gridy=0;
+		c.anchor=GridBagConstraints.WEST;
+		add(hslider,c);
+		c.gridy++;
+		add(sslider,c);
+		c.gridy++;
 		add(bslider,c);
 		
 		c.gridheight=5;
 		c.gridy++;
-		add(Box.createVerticalBox(),c);
+		c.weightx=1.0;
+		c.weighty=1.0;
+		add(new JLabel(""),c);
 		setBorder(new TitledBorder("Color selector"));
+		color=Color.BLUE;
 	}
-	
+
 	public void paintComponent(Graphics g)
 	{
+		super.paintComponents(g);
+		g.setColor(color);
+		g.fillRect(50, 50, 200, 200);
+	}
+	
+	class Listener implements ChangeListener
+	{
+
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			float hue=map((float)(hslider.getValue()),0.0f,360.0f,0.0f,1.0f);
+			float sat=map((float)(sslider.getValue()),0.0f,100.0f,0.0f,1.0f);
+			float bri=map(bslider.getValue(),0,100,0,1);
+			color=new Color(Color.HSBtoRGB(hue, sat, bri));
+			System.out.println(color);
+			repaint();
+		}
+		
+		private float map(float x,float in_min,float in_max,float out_min,float out_max)
+		{
+			 return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+			
+			
+		}
 		
 	}
 }
