b7fb0c0cd39ac4f7a8dd8814796d7420f07b614b
uplukaszp <uplukaszp1@gmail.com>
2017-01-21 23:52:06 +0100

    Color_Choosing_Panel changes
    Changed viev from RGB to HSB model.

--- src/CUSTOMCOLORS/Color_Choosing_Panel.java
+++ src/CUSTOMCOLORS/Color_Choosing_Panel.java
@@ -7,51 +7,60 @@ import javax.swing.border.TitledBorder;
 
 public class Color_Choosing_Panel extends JPanel{
 
-	JLabel rlab,glab,blab;
-	JTextArea rtext,gtext,btext;
-	JPanel colours;
-	Color c;
+	private JLabel htext,stext,btext;
+	private JSlider hslider,sslider,bslider;
+	private Color c;
 	public Color_Choosing_Panel() {
 		setBorder(new TitledBorder("Choose the color"));
-		rlab=new JLabel("R:");
-		glab=new JLabel("G:");
-		blab=new JLabel("B:");
-		rtext=new JTextArea(1, 4);
-		gtext=new JTextArea(1,4);
-		btext=new JTextArea(1,4);
-		colours=new JPanel();
-		colours.add(new JLabel(""));
+		
+		htext=new JLabel("Hue:");
+		stext=new JLabel("Saturation:");
+		btext=new JLabel("Brightness:");
+		
+		hslider=new JSlider(0,360,180);
+		sslider=new JSlider(0,100,50);
+		bslider=new JSlider(0,100,50);
 		
 		setLayout(new GridBagLayout());
 		GridBagConstraints c=new GridBagConstraints();
-		c.weightx=0;
-		c.weighty=0;
-		c.fill=GridBagConstraints.BOTH;
+		c.weightx=1.0;
+		c.weighty=1.0;
+		c.fill=GridBagConstraints.HORIZONTAL;
+		c.anchor=GridBagConstraints.NORTHWEST;
 		c.gridx=0;
 		c.gridy=0;
-		add(rlab,c);
+		add(htext,c);
 		c.gridx++;
-		add(rtext,c);
+		add(hslider,c);
+		
+		c=new GridBagConstraints();
+		c.weightx=1.0;
+		c.weighty=1.0;
+		c.fill=GridBagConstraints.HORIZONTAL;
+		c.anchor=GridBagConstraints.NORTHWEST;
+		c.gridx=0;
+		c.gridy=1;
+		add(stext,c);
 		c.gridx++;
-		add(glab,c);
-		c.gridx++;
-		add(gtext,c);
-		c.gridx++;
-		add(blab,c);
-		c.gridx++;
+		add(sslider,c);
+		
+		c=new GridBagConstraints();
+		c.weightx=1.0;
+		c.weighty=1.0;
+		c.fill=GridBagConstraints.HORIZONTAL;
+		c.anchor=GridBagConstraints.NORTHWEST;
+		c.gridx=0;
+		c.gridy=2;
 		add(btext,c);
-		//
-		c.weightx=100;
-		c.weighty=100;
-		c.gridx=1;
-		c.gridy+=2;
-		c.gridwidth=4;
-		c.gridheight=1;
-		add(colours,c);
-		colours.setBackground(Color.BLACK);
+		c.gridx++;
+		add(bslider,c);
+		
+		c.gridheight=5;
+		c.gridy++;
+		add(Box.createVerticalBox(),c);
 		setBorder(new TitledBorder("Color selector"));
 	}
-
+	
 	public void paintComponent(Graphics g)
 	{
 		
