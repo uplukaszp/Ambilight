6a73121239a7e3c60f1c2eb6c8b4439ca4262470
uplukaszp <uplukaszp1@gmail.com>
2017-01-30 17:11:46 +0100

    Removed Rand, added HSV previev
    Unnecessary  random settings was removed.
    Added better HSV preview

--- src/CUSTOMCOLORS/Color_Choosing_Panel.java
+++ src/CUSTOMCOLORS/Color_Choosing_Panel.java
@@ -25,11 +25,10 @@ public class Color_Choosing_Panel extends JPanel{
 		
 		setLayout(new GridBagLayout());
 		GridBagConstraints c=new GridBagConstraints();
-		c.weightx=1.0;
+		c.weightx=0.3;
 		c.weighty=0;
-		//c.fill=GridBagConstraints.HORIZONTAL;
-		c.anchor=GridBagConstraints.EAST;
 		
+		c.anchor=GridBagConstraints.EAST;
 		c.gridx=0;
 		c.gridy=0;
 		add(htext,c);
@@ -40,6 +39,8 @@ public class Color_Choosing_Panel extends JPanel{
 		c.gridx=1;
 		c.gridy=0;
 		c.anchor=GridBagConstraints.WEST;
+		c.fill=GridBagConstraints.HORIZONTAL;
+		c.weightx=0.7;
 		add(hslider,c);
 		c.gridy++;
 		add(sslider,c);
@@ -52,14 +53,32 @@ public class Color_Choosing_Panel extends JPanel{
 		c.weighty=1.0;
 		add(new JLabel(""),c);
 		setBorder(new TitledBorder("Color selector"));
-		color=Color.BLUE;
+		setColor(new Color(Color.HSBtoRGB(getHUE(), getSaturation(), getBrightness())));
 	}
 	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponents(g);
+		Graphics g2=hslider.getGraphics();
+		Dimension sizeOfPanel=this.getSize();
+		Rectangle previewRectangle=new Rectangle((int)(sizeOfPanel.width*0.2),(int)(sizeOfPanel.height*0.55),(int)(sizeOfPanel.width*0.6),(int)(sizeOfPanel.width*0.6));
+		
+		for(int x=0;x<previewRectangle.getWidth();x++)
+		{
+			g.setColor(new Color(Color.HSBtoRGB(map((float)(x),0.0f,(float)(previewRectangle.getWidth()),0.0f,1.0f), getSaturation(), getBrightness())));
+			g.drawLine(previewRectangle.x+x, 100, previewRectangle.x+x, 110);
+			
+			g.setColor(new Color(Color.HSBtoRGB(getHUE(),map((float)(x),0.0f,(float)(previewRectangle.getWidth()),0.0f,1.0f), getBrightness())));
+			g.drawLine(previewRectangle.x+x, 115, previewRectangle.x+x, 125);
+			
+			g.setColor(new Color(Color.HSBtoRGB(getHUE(),getSaturation(), map((float)(x),0.0f,(float)(previewRectangle.getWidth()),0.0f,1.0f))));
+			g.drawLine(previewRectangle.x+x, 130, previewRectangle.x+x, 140);
+			
+			
+		}
+		
 		g.setColor(color);
-		g.fillRect(50, 50, 200, 200);
+		g.fillRect(previewRectangle.x, previewRectangle.y, previewRectangle.width, previewRectangle.height);
 	}
 	public void addListener(ChangeListener l)
 	{
--- src/CUSTOMCOLORS/Custom_Color_Panel.java
+++ src/CUSTOMCOLORS/Custom_Color_Panel.java
@@ -3,61 +3,30 @@ package CUSTOMCOLORS;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class Custom_Color_Panel extends JPanel {
 	Color_Choosing_Panel colorch;
-	Color_Type_Of_Change_Panel typeofch;
 	Ext_Prev_Panel prev;
-	Random_Settings rand;
 	
 	public Custom_Color_Panel() {
 		setLayout(new GridBagLayout());
 		GridBagConstraints c=new GridBagConstraints();
-		c.weightx=0;
-		c.weighty=0;
-		c.anchor=GridBagConstraints.WEST;
 		c.gridx=0;
 		c.gridy=0;
-		
+		c.weightx=0.15;
+		c.weighty=100;
+		c.fill=GridBagConstraints.BOTH;
 		colorch=new Color_Choosing_Panel();
 		colorch.addListener(new ColorChooseListener());
-		
-		typeofch=new Color_Type_Of_Change_Panel();
-		typeofch.addListener(new TypeOfChangeListener());
 		prev=new Ext_Prev_Panel(this);
-		rand=new Random_Settings();
 		
-		
-		c.fill=GridBagConstraints.BOTH;
-		c.gridwidth=4;
-		c.gridheight=2;
-		add(typeofch,c);
-		
-		
-		c.weighty=100;
-		c.gridx+=2;
-		c.gridx-=2;
-		c.gridy=2;
 		add(colorch,c);
-		add(rand,c);
-		colorch.setVisible(false);
-		
-		c.weightx=100;
-		c.gridwidth=4;
-		c.gridheight=4;
-		c.gridx=4;
-		c.gridy=0;
-		
-	
+		c.weightx=0.75;
+		c.gridx++;
 		add(prev,c);
-		colorch.setSize(typeofch.getSize());
 	}
 	
 	class ColorChooseListener implements ChangeListener
@@ -68,32 +37,6 @@ public class Custom_Color_Panel extends JPanel {
 			colorch.setColor(new Color(Color.HSBtoRGB(colorch.getHUE(), colorch.getSaturation(), colorch.getBrightness())));
 			repaint();
 		}
-		
-		
-	}
-	class TypeOfChangeListener implements ActionListener
-	{
-
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			JRadioButton source=(JRadioButton)(arg0.getSource());
-			if(source.getText().contains("Random"))
-			{
-				colorch.setVisible(false);
-				rand.setVisible(true);
-				
-			}
-			if(source.getText().contains("Single")||source.getText().contains("Custom"))
-			{
-				colorch.setVisible(true);
-				rand.setVisible(false);
-			}
-			colorch.setSize(rand.getSize());
-			repaint();
-			
-		}
-
-		
 	}
 	
 }
